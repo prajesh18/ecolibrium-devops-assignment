6bb4602bc55f72766d32ea76ed5f5a28fbe001a2
prajesh18 <95608609+prajesh18@users.noreply.github.com>
2026-04-09 22:12:31 +0530

    Add backend config to provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">= 1.3.0"
+  backend "s3" {
+    bucket         = "ecolibrium-terraform-state"
+    key            = "eks/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
