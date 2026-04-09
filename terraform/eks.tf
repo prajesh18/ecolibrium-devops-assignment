ec2cd66555417a7e029571f605306641b5c2a56f
prajesh18 <95608609+prajesh18@users.noreply.github.com>
2026-04-09 22:27:45 +0530

    Update the instance_type in eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -25,7 +25,7 @@ resource "aws_eks_node_group" "main" {
   node_role_arn   = aws_iam_role.eks_node_role.arn
 
   subnet_ids = aws_subnet.private[*].id
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.small"]
 
   scaling_config {
     desired_size = 1
